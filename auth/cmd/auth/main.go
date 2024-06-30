66d9453d6bb2c01da4fcd805aed083aa80ad114b
Slava <Slava.lokshin@mail.ru>
2024-06-30 17:40:45 +0300

    Добавлен логгер

--- auth/cmd/auth/main.go
+++ auth/cmd/auth/main.go
@@ -4,13 +4,18 @@ import (
 	"fmt"
 
 	"github.com/viacheslavek/grpcauth/auth/internal/config"
+	"github.com/viacheslavek/grpcauth/auth/internal/lib/logger"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
-	fmt.Printf("%+v", cfg)
-	// TODO: логгер
+	fmt.Printf("%+v\n", cfg)
+
+	lg := logger.SetupLogger(cfg.Env)
+	lg.Info("lalal")
+	lg.Warn("pupu")
+
 	// TODO: приложение
 	// TODO: запуск
 }
